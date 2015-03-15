9fda5547ee66f11b1e552c53b14125a1f4d4a55c
Bill Hazard <bhazard2@gmail.com>
2015-03-15 19:36:46 -0400

    Changed download URL and cleaned up the attributes

--- wso2/recipes/as.rb
+++ wso2/recipes/as.rb
@@ -11,8 +11,8 @@ include_recipe 'zip'
 include_recipe 'java'
 
 wso2_component 'wso2as' do
-  tarball_url node['wso2']['wso2as_tarball_url']
+  tarball_url node['wso2']['as']['tarball_url']
   download_dir node['wso2']['download_dir']
-  install_dir node['wso2']['wso2as_install_dir']
+  install_dir node['wso2']['as']['install_dir']
   init_script node['wso2']['init_script']
 end
