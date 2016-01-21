4cc014aa12d78aee85c763eca01f3ae0c921a494
Bill Hazard <bhazard2@gmail.com>
2016-01-21 08:54:35 -0500

    Added composer to joomla cookbook

--- joomla/recipes/php.rb
+++ joomla/recipes/php.rb
@@ -13,3 +13,14 @@ package 'php5-mcrypt'
 
 # Probably do not need this, but handy
 package 'php5-cli'
+
+# Composer is used to install dependencies for php-github-api.  This lib is
+# used by the admin site.  There are probably better ways to include the lib
+# as part of the Joomla! component?  Or perhaps use a joomla-specific solution.
+package 'curl'
+package 'php5-curl'
+
+bash 'Install composer' do
+  code 'curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer'
+  not_if "which composer"
+end
